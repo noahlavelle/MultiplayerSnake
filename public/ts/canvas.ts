2ffa1afd2f3ae9bac14d7d379c215ca3a07ae36e
BuildTools <noah.lavelle@gmail.com>
2021-02-01 18:49:29 +0000

    more ui tweaks

--- public/ts/canvas.ts
+++ public/ts/canvas.ts
@@ -1,19 +1,16 @@
 const canvas = <HTMLCanvasElement> document.getElementById('gamecanvas');
 const ctx = canvas.getContext('2d');
 let keyPressed = false;
-
 function draw(x, y, gridSize, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, gridSize, gridSize);
 }
-
 function clear() {
     ctx.fillStyle = localStorage.getItem("canvas-color") || "#ffffff";
     ctx.fillRect(0, 0, 990, 990);
 }
-
 function text(text, font, color, xOffset, yOffset) {
     ctx.fillStyle = color;
-    ctx.font = "25px "+font;
-    ctx.fillText(text, xOffset, yOffset); 
+    ctx.font = "25px " + font;
+    ctx.fillText(text, xOffset, yOffset);
 }
