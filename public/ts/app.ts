5323d1b961e12f0fa47898d43d176f8062d43533
BuildTools <noah.lavelle@gmail.com>
2021-02-02 11:07:49 +0000

    added mobile styling and organised app.js with classes

--- public/ts/app.ts
+++ public/ts/app.ts
@@ -4,209 +4,239 @@ const errorCodes = ["?a", "?b"]
 
 let game : Game;
 
-function reset () {
-    hideAll();
-    if (pages.includes(location.hash)) document.title = titles[pages.indexOf(location.hash)];
-    if (location.hash == "") {
-        $('#home').show();
-    } else {
-        $(location.hash).show();
+class LinkHandling {
+    errorHandling : ErrorHandling;
+
+    constructor () {
+        this.eventListner();
+        this.errorHandling = new ErrorHandling();
     }
 
-    if (game != undefined && location.hash != "#game") {
-        game.running = false;
-        setTimeout(() => {
-            socket.emit("send-data", ["", "", []]);
-        }, 100)
-        
-    }
-}
+    eventListner() {
+        this.handle();
 
-if (location.search) {
-    let error = location.search;
-    history.replaceState(null, null, '/');
-    if (error == '?a') {
-        // @ts-ignore
-        Swal.fire({
-            icon: 'error',
-            title: '404 Error',
-            text: 'The requested file cannot be found',
-          })
-    }
-    else if (error == '?b') {
-        // @ts-ignore
-        Swal.fire({
-            icon: 'error',
-            title: 'Game not found',
-          })
-    }
-    else if (error == '?c') {
-        // @ts-ignore
-        Swal.fire({
-            icon: 'error',
-            title: 'The Game has timed out'
-        });
-    };
-};
-
-jQuery(() => {
-    let playerColor : any = localStorage.getItem("player-color");
-    let canvasColor : any = localStorage.getItem("canvas-color");
-    let foodColor : any = localStorage.getItem("food-color");
-
-    onresize = () => {
-        if (innerHeight > innerWidth) {
-            document.getElementById('gamecanvas').style.width = '80vw';
-            document.getElementById('gamecanvas').style.height = '80vw';
+        window.onpopstate = () => {
+            this.handle();
+            this.errorHandling.handle();
         }
-        else {
-            document.getElementById('gamecanvas').style.width = '80vh';
-            document.getElementById('gamecanvas').style.height = '80vh';
+    }
+
+    handle () {
+        this.hideAll();
+        if (pages.includes(location.hash)) document.title = titles[pages.indexOf(location.hash)];
+        if (location.hash == "") {
+            $('#home').show();
+        } else {
+            $(location.hash).show();
         }
-    };
     
-    // @ts-ignore
-    onresize();
-
-    reset();
-
-    window.onpopstate = () => {
-        reset();
+        if (game != undefined && location.hash != "#game") {
+            game.running = false;
+            setTimeout(() => {
+                socket.emit("send-data", ["", "", []]);
+            }, 100)
+            
+        }
     }
 
-    $("#color-pickers").children().toArray().forEach((child : any) => {
-        let currentColor : any = null;
-        switch (child.getAttribute("value")) {
-            case "player-color":
-                if (playerColor == null) {
-                    playerColor = child.getAttribute("default");
-                }
-                currentColor = playerColor;
-            break;
-            case "canvas-color":
-                if (canvasColor == null) {
-                    canvasColor = child.getAttribute("default");
-                }
-                currentColor = canvasColor;
-            break;
-            case "food-color":
-                if (foodColor == null) {
-                    foodColor = child.getAttribute("default");
-                }
-                currentColor = foodColor;
-            break;
-        }
-
-        const newElement = document.createElement("span");
-        child.append(newElement);
-
-        // @ts-ignore
-        const pickr = Pickr.create({
-            el: newElement,
-            theme: 'classic', // or 'monolith', or 'nano'
-            default: currentColor,
-        
-            swatches: [
-                'rgba(244, 67, 54, 1)',
-                'rgba(233, 30, 99, 1)',
-                'rgba(156, 39, 176, 1)',
-                'rgba(103, 58, 183, 1)',
-                'rgba(63, 81, 181, 1)',
-                'rgba(33, 150, 243, 1)',
-                'rgba(3, 169, 244, 1)',
-                'rgba(0, 188, 212, 1)',
-                'rgba(0, 150, 136, 1)',
-                'rgba(76, 175, 80, 1)',
-                'rgba(139, 195, 74, 1)',
-                'rgba(205, 220, 57, 1)',
-                'rgba(255, 235, 59, 1)',
-                'rgba(255, 193, 7, 1)'
-            ],
-        
-            components: {
-        
-                // Main components
-                preview: true,
-                opacity: false,
-                hue: true,
-        
-                // Input / output Options
-                interaction: {
-                    hex: true,
-                    rgba: true,
-                    hsla: true,
-                    hsva: true,
-                    cmyk: true,
-                    input: true,
-                    clear: true,
-                    save: true
-                }
-            }
+    hideAll () {
+        pages.forEach((page) => {
+            $(page).hide();
         });
-
-        pickr.on('save', (color : any) => {
-            localStorage.setItem(child.getAttribute("value"), color.toHEXA().toString())
-        });
-    });
-});
-
-$("body").on("mousemove", (event) => {
-    $(".background").css("background-position", `${event.clientX * 0.033}% ${event.clientY * 0.033}%`);
-});
-
-function hideAll () {
-    pages.forEach((page) => {
-        $(page).hide();
-    });
+    }
 }
 
-$("#joingame").on("click", () => {
-    // @ts-ignore
-    Swal.fire({
-        title: 'Enter Game ID',
-        input: 'text',
-        inputAttributes: {
-          autocapitalize: 'off'
-        },
-        showCancelButton: true,
-        confirmButtonText: 'Look up',
-        showLoaderOnConfirm: true,
-        preConfirm: () => {
-            // @ts-ignore
-            let id : any = $(Swal.getInput()).val();
-            return new Promise( (resolve, reject) => {
-                socket.emit("gameslist")
-                socket.on("gameslist", (gameslist) => {
-                    if (!gameslist.includes(id)) {
-                        reject();
-                    } else {
-                        resolve(id);
-                    }
-                })
-            }).catch(() => {
-                // @ts-ignore
-                Swal.showValidationMessage(
-                    `Request failed: Game not found`
-                )
-            })
-        },
-        allowOutsideClick: true
-    }).then((result) => {
-        if (result.isConfirmed) {
-            if (result != undefined) {
-                socket.emit("joingame", result.value);
-                location.hash = "#game"
-                game = new Game;
-            }
-        }
-    });
-});
+class ErrorHandling {
+    constructor() {
+        this.handle();
+    }
 
-$("#play").on("click", () => {
-    // @ts-ignore
-    socket.emit("creategame", 100 / ((Number.parseInt($("#gamespeed").val())) / 100), Number.parseInt($("#gametime").val()), Boolean($("#getlength").val()));
-    game = new Game;
-})
+    handle () {
+        if (location.search) {
+            let error = location.search;
+            history.replaceState(null, null, '/');
+            if (error == '?a') {
+                // @ts-ignore
+                Swal.fire({
+                    icon: 'error',
+                    title: '404 Error',
+                    text: 'The requested file cannot be found',
+                  })
+            }
+            else if (error == '?b') {
+                // @ts-ignore
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Game not found',
+                  })
+            }
+            else if (error == '?c') {
+                // @ts-ignore
+                Swal.fire({
+                    icon: 'error',
+                    title: 'The Game has timed out'
+                });
+            };
+        };
+    }
+}
+
+class ColorHandling {
+    playerColor : any;
+    canvasColor : any;
+    foodColor : any;
+
+    constructor () {
+        this.playerColor = localStorage.getItem("player-color");
+        this.canvasColor = localStorage.getItem("canvas-color");
+        this.foodColor = localStorage.getItem("food-color");
+
+        this.createPickers();
+    }
+    
+    createPickers () {
+        $("#color-pickers").children().toArray().forEach((child : any) => {
+            let currentColor : any = null;
+            switch (child.getAttribute("value")) {
+                case "player-color":
+                    if (this.playerColor == null) {
+                        this.playerColor = child.getAttribute("default");
+                    }
+                    currentColor = this.playerColor;
+                break;
+                case "canvas-color":
+                    if (this.canvasColor == null) {
+                        this.canvasColor = child.getAttribute("default");
+                    }
+                    currentColor = this.canvasColor;
+                break;
+                case "food-color":
+                    if (this.foodColor == null) {
+                        this.foodColor = child.getAttribute("default");
+                    }
+                    currentColor = this.foodColor;
+                break;
+            }
+    
+            const newElement = document.createElement("span");
+            child.append(newElement);
+    
+            // @ts-ignore
+            const pickr = Pickr.create({
+                el: newElement,
+                theme: 'classic', // or 'monolith', or 'nano'
+                default: currentColor,
+            
+                swatches: [
+                    'rgba(244, 67, 54, 1)',
+                    'rgba(233, 30, 99, 1)',
+                    'rgba(156, 39, 176, 1)',
+                    'rgba(103, 58, 183, 1)',
+                    'rgba(63, 81, 181, 1)',
+                    'rgba(33, 150, 243, 1)',
+                    'rgba(3, 169, 244, 1)',
+                    'rgba(0, 188, 212, 1)',
+                    'rgba(0, 150, 136, 1)',
+                    'rgba(76, 175, 80, 1)',
+                    'rgba(139, 195, 74, 1)',
+                    'rgba(205, 220, 57, 1)',
+                    'rgba(255, 235, 59, 1)',
+                    'rgba(255, 193, 7, 1)'
+                ],
+            
+                components: {
+            
+                    // Main components
+                    preview: true,
+                    opacity: false,
+                    hue: true,
+            
+                    // Input / output Options
+                    interaction: {
+                        hex: true,
+                        rgba: true,
+                        hsla: true,
+                        hsva: true,
+                        cmyk: true,
+                        input: true,
+                        clear: true,
+                        save: true
+                    }
+                }
+            });
+    
+            pickr.on('save', (color : any) => {
+                try {
+                    localStorage.setItem(child.getAttribute("value"), color.toHEXA().toString())
+                } catch {};
+            });
+        });
+    }
+}
+
+class SpecialButtons {
+    constructor () {
+        this.eventHandlers()
+    }
+
+    eventHandlers() {
+        $("#joingame").on("click", () => {
+            this.joinGame();
+        });
+        
+        $("#play").on("click", () => {
+            this.play();
+        });
+    }
+
+    joinGame() {
+        // @ts-ignore
+        Swal.fire({
+            title: 'Enter Game ID',
+            input: 'text',
+            inputAttributes: {
+                autocapitalize: 'off'
+            },
+            showCancelButton: true,
+            confirmButtonText: 'Look up',
+            showLoaderOnConfirm: true,
+            preConfirm: () => {
+                // @ts-ignore
+                let id : any = $(Swal.getInput()).val();
+                return new Promise( (resolve, reject) => {
+                    socket.emit("gameslist")
+                    socket.on("gameslist", (gameslist) => {
+                        if (!gameslist.includes(id)) {
+                            reject();
+                        } else {
+                            resolve(id);
+                        }
+                    })
+                }).catch(() => {
+                    // @ts-ignore
+                    Swal.showValidationMessage(
+                        `Request failed: Game not found`
+                    )
+                })
+            },
+            allowOutsideClick: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                if (result != undefined) {
+                    socket.emit("joingame", result.value);
+                    location.hash = "#game"
+                    game = new Game;
+                }
+            }
+        });
+    }
+
+    play() {
+        // @ts-ignore
+        socket.emit("creategame", 100 / ((Number.parseInt($("#gamespeed").val())) / 100), Number.parseInt($("#gametime").val()), Boolean($("#getlength").val()));
+        game = new Game;
+    }
+}
 
 
 class Slider {
@@ -239,6 +269,19 @@ class Slider {
     }
 }
 
+
+jQuery(() => {
+    const linkHandling = new LinkHandling ();
+    const colorHandling = new ColorHandling ();
+    const specialButtons = new SpecialButtons();
+});
+
 $("#creategame > div > .options").children().toArray().forEach((child) => {
-    const slider : Slider = new Slider(child);
+    if ($(child).find("input[type=range]") != null && !$(child).hasClass("buttons")) {
+        const slider : Slider = new Slider(child);
+    }
+});
+
+$("body").on("mousemove", (event) => {
+    $(".background").css("background-position", `${event.clientX * 0.033}% ${event.clientY * 0.033}%`);
 });
